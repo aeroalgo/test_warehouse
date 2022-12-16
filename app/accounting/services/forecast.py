0ffd0356a78138822aa8dac4c4300f97dc949a7e
Andrey Erokhin <aeroalgo@gmail.com>
2022-12-17 01:52:58 +0300

    Добавлено отображение ближайших заканчивающихся товаров.

--- app/accounting/services/forecast.py
+++ app/accounting/services/forecast.py
@@ -1,6 +1,21 @@
-import numpy as np
-from datetime import timedelta
+from curses.ascii import isdigit
 
+import numpy as np
+from datetime import timedelta, datetime
+
+from pydantic import BaseModel, Field
+from typing import *
+
+
+class ForecastModel(BaseModel):
+    """Класс данных для линейного графика"""
+    id: int
+    article: str
+    show_adding: bool
+    count: int
+    last_update: datetime
+    end_date: datetime
+    results: List[int]
 
 class Forecast:
 
@@ -11,7 +26,10 @@ class Forecast:
     def get_forecast(self):
         for data in self.datas:
             if data.sku.article not in self.sku_data.keys():
-                self.sku_data[data.sku.article] = {"count": [data.count]}
+                self.sku_data[data.sku.article] = {
+                    "count": [data.count],
+                    "id": data.sku.id
+                }
                 self.sku_data[data.sku.article].update({"actual_date": [data.actual_date]})
             else:
                 self.sku_data[data.sku.article]["count"].append(data.count)
@@ -37,12 +55,16 @@ class Forecast:
                 count += 1
             results.append(result)
             end_date = article_data.get("actual_date")[-1] + timedelta(hours=3 * count)
+            alert_date = article_data.get("actual_date")[-1] + timedelta(hours=12)
             sku_forecast = {
                 "article": article,
+                "show_adding": end_date <= alert_date,
+                "id": article_data.get("id"),
                 "count": count_product[-1],
                 "last_update": article_data.get("actual_date")[-1],
                 "end_date": end_date,
                 "results": results
             }
-            forecasts.append(sku_forecast)
+
+            forecasts.append(ForecastModel(**sku_forecast).dict())
         return forecasts
